a11d78eb804c189d1e196d34c6c0e60f874efe54
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-03-17 14:35:06 +0100

    Refactor code to be more modular

---
+++ core.js
@@ -0,0 +1,146 @@
+'use strict';
+
+var BaseConverter = {};
+
+BaseConverter.convert = function(from, to, number) {
+    var isComma = isComma || false;
+    var results = [];
+
+    to = Array.isArray(to) ? to : [to];
+    if (BaseConverter.validateNumber(number, from)) {
+        if (from == 10) {
+            for (var i = 0; i < to.length; i++) {
+                results[i] = fromBaseTen(number, to[i]);
+            }
+        } else {
+            let baseTenNumber = toBaseTen(number, from);
+
+            for (var i = 0; i < to.length; i++) {
+                results[i] = fromBaseTen(baseTenNumber, to[i]);
+            }
+        }
+    } else {
+        return false;
+    }
+    return results;
+
+    function toBaseTen(number, base) {
+        var digits = [],
+            sum = 0;
+        var split = number.split(",");
+
+        for (var i = 0; i < split[0].length; i++) {
+            sum += BaseConverter.getDigitValue(split[0][i]) * Math.pow(base, split[0].length - 1 - i);
+        }
+
+        if (split[1]) {
+            sum += ',';
+            var temp = 0;
+            for (var i = 0; i < split[1].length; i++) {
+                temp += BaseConverter.getDigitValue(split[1][i]) * Math.pow(base, -(i + 1));
+            }
+            sum += temp.toString().substring(2);
+        }
+
+        return sum.toString();
+    }
+
+    function fromBaseTen(number, base) {
+        var potenzen = [],
+            rest = '',
+            ergebnis = '';
+
+        var split = number.toString().split(","),
+            digits = number.toString().split(''),
+            help = 0;
+
+        potenzen = _makePowers(split[0], base);
+
+        // If given number is a number with a comma then loop loop twice. One time for 'body' and one time for the 'comma'
+        rest = split[0];
+        for (var i = (potenzen.length-1); i >= 0; i--) {
+            if (rest == 0 || (rest - potenzen[i] < 0)) {
+                ergebnis += '0';
+            } else {
+                help = Math.floor(rest / potenzen[i]);
+                ergebnis += BaseConverter.getDigitCharacter(help).toString();
+                rest = rest % potenzen[i];
+            }
+        }
+
+        // calculates the comma
+        if (split[1]) {
+            ergebnis += ","
+            rest = Number('0.' + split[1]);
+            for (var i = 1; i <= 8; i++) {
+                if (rest == 0 || (rest - Math.pow(base, -i) < 0)) {
+                    ergebnis += '0';
+                } else {
+                    help = Math.floor(rest / Math.pow(base, -i));
+                    ergebnis += BaseConverter.getDigitCharacter(help).toString();
+                    rest = rest % Math.pow(base, -i);
+                }
+            }
+        }
+        // Rechnet die nötigen Potenzen der Basis, welche für die Umrechnung von nöten sind aus.
+        function _makePowers(number, base) {
+            var i = 0,
+                powerArr = [];
+
+            do {
+                powerArr.push(Math.pow(base, i));
+                i++;
+            } while (Math.pow(base, i) <= number);
+
+            return powerArr;
+        }
+
+        return ergebnis;
+    }
+};
+
+/** This function gets a base 10 number and return the corresponding digit to that value.
+ * E.g. 'E' for 14
+ */
+BaseConverter.getDigitCharacter = function(number) {
+    return BaseConverter.digits[number];
+};
+
+BaseConverter.getDigitValue = function(digit) {
+    return BaseConverter.digits.indexOf(digit.toUpperCase());
+}
+
+BaseConverter.validateNumber = function(number, base) {
+    var isComma = isComma || false;
+    var digits = number.split("");
+
+    // When the backspace key is pressed...
+    if (number == -1) {
+        // ...test if comma was deleted
+        if (inputField.value.indexOf(",") == -1) {
+            isComma = false;
+        }
+        return true;
+    }
+
+    // Loop throught the received number and check every digit for its validness
+    for (var i = 0; i < digits.length; i++) {
+        if (digits[i] == "," && isComma == false) {
+            isComma = true;
+            return true;
+        } else if (digits[i] == "," && isComma == true) {
+            return false;
+        }
+
+        // IDEA: Use foreach() and declare this variable outside of the for loop
+        let digitValue = BaseConverter.getDigitValue(digits[i]);
+
+        if (digitValue >= selectedBase || digitValue == -1) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// Array that maps all the digits from bases 2-36
+BaseConverter.digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
--- main.js
+++ main.js
@@ -1,113 +1,94 @@
-var ergebnis = '';
-var addedCharacter = "";
+'use strict';
 
-var isComma = false;
-var selectedBase = 10;
-var elem = document.getElementById('number');
-var base = document.getElementById('base');
-var addBaseButton = document.getElementById('add-base');
-document.onload = base.value = 10;
+var resultBox = document.getElementsByClassName('result-number'),
+    resultBoxBase = [];
 
-addBaseButton.addEventListener("click", addResultBox);
+var addedCharacter = "",
+    selectedBase,
+    inputField = document.getElementById('number');
 
-base.addEventListener("change", function updateBase(e) {
-    if (elem.value != "") {
-      console.log('HERE');
-        if (selectedBase == 10) {
-          console.log('HERE TOO');
-            sum = fromBaseTen(elem.value, this.value);
-            console.log(sum);
-        } else {
-            let help = toBaseTen(elem.value, selectedBase);
-            sum = fromBaseTen(help, this.value);
-        }
-        elem.value = sum;
-    } else {
-        elem.value = "";
-    }
+window.onload = function() {
+    base.value = 10;
+    selectedBase = 10;
+    inputField.value = "";
+    updateResultBoxArray();
+};
 
-    // Update to new Base
-    selectedBase = this.value;
+// Initialize eventlistener
+document.getElementById('add-base').addEventListener("click", addResultBox);
+document.getElementById('base').addEventListener("change", updateBase);
+inputField.addEventListener("paste", handlePaste);
 
-    elem.placeholder = "Use digits 0-" + getDigitCharacter(selectedBase - 1);
-});
-elem.addEventListener("paste", function paste(e) {
-    e.preventDefault();
-    var pastedData = e.clipboardData.getData('text');
+// Is needed to detect which characters were added to the input field
+// NOTE: In future it is possible to use InputEvent.data to get the added character, so this eventlistener would be obsolete. See input eventlistener.
+inputField.addEventListener("keypress", keyInput);
 
-    if (validateNumber(pastedData, selectedBase) != -1) {
-        this.value = this.value + pastedData;
-    } else {
-        console.error("The pasted data does not match with the selected base!");
-    }
+// Handles all input to the number input field
+// NOTE: In future it is possible to use InputEvent.data to get the added character, so it would not be necessary to use an keypress eventlistener
+inputField.addEventListener("input", inputHandler);
 
-});
-
-elem.addEventListener("keypress", function char(e) {
+function keyInput(e) {
     if (e.charCode == 0) {
         addedCharacter = -1;
     } else {
         addedCharacter = String.fromCharCode(e.charCode);
     }
-});
-elem.addEventListener("input", function calculate(e) {
-    e.preventDefault(); // Does not work :(
-    e.stopPropagation();
-    // NOTE: In future it is possible to use InputEvent.data to get the added character, so it would not be necessary to use an keypress eventlistener
+}
+
+function inputHandler(e) {
     let digit = addedCharacter;
-    console.log("addedCharacter: " + addedCharacter);
+    // create results
+    let results = BaseConverter.convert(selectedBase, resultBoxBase, this.value);
 
-    if (validateNumber(digit, selectedBase) != -1) {
-        /* Convert and insert the values*/
-        var baseTen = toBaseTen(this.value, selectedBase);
-        var resultBase = document.getElementsByClassName('result-base');
-
-        console.log(resultBase[0].value);
-        for (var i = 0; i < resultBase.length; i++) {
-            let sum = 0,
-                curr = resultBase[i].value;
-            console.log(curr);
-
-            if (selectedBase == 10) {
-                sum = fromBaseTen(this.value, curr);
-            } else {
-                sum = fromBaseTen(baseTen, curr);
-            }
-
-            document.getElementsByClassName('result-number')[i].innerHTML = sum;
-        }
+    // add results to the page
+    if (results == false) {
+        console.error("Character not allowed. Use digits 0-"+ (selectedBase-1));
     } else {
-        var v = this.value.split("");
-        v.splice(this.selectionEnd - 1, 1);
-        this.value = v.join("");
+        for (var i = 0; i < resultBox.length; i++) {
+            resultBox[i].innerHTML = results[i];
+        }
     }
-});
+}
+
+function updateBase(e) {
+    if (inputField.value != "") {
+        inputField.value = BaseConverter.convert(selectedBase, this.value, inputField.value);
+    }
+
+    // Update to new Base
+    selectedBase = this.value;
+    inputField.placeholder = "Use digits 0-" + BaseConverter.getDigitCharacter(selectedBase - 1);
+}
+
+function handlePaste(e) {
+    e.preventDefault();
+    var pastedData = e.clipboardData.getData('text');
+
+    if (BaseConverter.validateNumber(pastedData, selectedBase)) {
+        this.value = this.value + pastedData;
+    } else {
+        console.error("Pasted data is not allowed. Try to change the format");
+    }
+}
 
 function enterEditMode(elem) {
     if (elem.parentNode.className !== "expanded") {
         elem.parentNode.className = "expanded";
+        elem.focus();
     }
 }
 
-function leaveEditMode(elem) {
-    if (elem.parentNode.className == "expanded") {
-        elem.parentNode.className = "";
+function leaveEditMode(button) {
+    if (button.parentNode.className == "expanded") {
+        button.parentNode.className = "";
     }
-}
-
-function toggleEditMode(elem) {
-    console.log(elem);
-    if (elem.parentNode.className == 'expanded') {
-        elem.parentNode.className = "";
-        elem.blur();
-    } else {
-        elem.parentNode.setAttribute('class', 'expanded');
-        elem.select();
-    }
+    updateResultBoxArray();
+    inputField.focus();
 }
 
 function removeResultBox(elem) {
     elem.parentNode.parentNode.removeChild(elem.parentNode);
+    updateResultBoxArray();
 }
 
 function addResultBox(e) {
@@ -134,7 +115,7 @@ function addResultBox(e) {
     baseElement.setAttribute('class', 'result-base');
     baseElement.setAttribute('type', 'text');
     baseElement.setAttribute('value', '--');
-    baseElement.setAttribute('onfocus', 'toggleEditMode(this);')
+    baseElement.setAttribute('onfocus', 'enterEditMode(this);')
     textElement.setAttribute('class', 'result-number');
 
     baseElement.innerHTML = baseValue;
@@ -148,126 +129,17 @@ function addResultBox(e) {
     console.log(newListItem);
     document.getElementsByClassName('results')[0].appendChild(newListItem);
 
-    baseElement.select();
+    baseElement.focus();
 }
 
+function updateResultBoxArray() {
+    for (var i = 0; i < resultBox.length; i++) {
+        resultBoxBase[i] = document.getElementsByClassName('result-base')[i].value;
+    }
+}
+
+//----- BASE CONVERTER CORE -----//
 /**
 This function receives a number and the base from which the number comes from.
 It then chops the received number apart and multiplies the value of the digit with the place value to produce the base ten result.
 */
-function toBaseTen(number, base) {
-    var digits = [],
-        sum = 0;
-    var split = number.split(",");
-
-    for (var i = 0; i < split[0].length; i++) {
-        sum += getDigitValue(split[0][i]) * Math.pow(base, split[0].length - 1 - i);
-    }
-
-    if (split[1]) {
-        sum += ',';
-        var temp = 0;
-        for (var i = 0; i < split[1].length; i++) {
-            temp += getDigitValue(split[1][i]) * Math.pow(base, -(i + 1));
-        }
-        sum += temp.toString().substring(2);
-    }
-
-    return sum.toString();
-}
-
-function fromBaseTen(number, base) {
-    var potenzen = [],
-        rest = '',
-        ergebnis = '';
-
-    console.log("number: " + number);
-    var split = number.toString().split(","),
-        digits = number.toString().split('');
-
-    potenzen = _makePowers(split[0], base);
-
-    // If given number is a number with a comma then loop loop twice. One time for 'body' and one time for the 'comma'
-    rest = split[0];
-    for (var i = potenzen.length - 1; i >= 0; i--) {
-        if (rest == 0 || (rest - potenzen[i] < 0)) {
-            ergebnis += '0';
-        } else {
-            help = Math.floor(rest / potenzen[i]);
-            ergebnis += getDigitCharacter(help).toString();
-            rest = rest % potenzen[i];
-        }
-    }
-
-    // calculates the comma
-    if (split[1]) {
-        ergebnis += ","
-        rest = Number('0.' + split[1]);
-        for (var i = 1; i <= 8; i++) {
-            if (rest == 0 || (rest - Math.pow(base, -i) < 0)) {
-                ergebnis += '0';
-            } else {
-                help = Math.floor(rest / Math.pow(base, -i));
-                ergebnis += getDigitCharacter(help).toString();
-                rest = rest % Math.pow(base, -i);
-            }
-        }
-    }
-
-
-    // Rechnet die nötigen Potenzen der Basis, welche für die Umrechnung von nöten sind aus.
-    function _makePowers(number, base) {
-        var i = 0,
-            powerArr = [];
-
-        do {
-            powerArr.push(Math.pow(base, i));
-            i++;
-        } while (Math.pow(base, i) <= number);
-
-        return powerArr;
-    }
-
-    return ergebnis;
-}
-
-/** This function gets a base 10 number and return the corresponding digit to that value.
- * E.g. 'E' for 14
- */
-function getDigitCharacter(number) {
-    var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-    return digits[number];
-}
-
-function getDigitValue(digit) {
-    var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-    return digits.indexOf(digit.toUpperCase());
-}
-
-function validateNumber(number, base) {
-    if (number == -1) {
-        if (elem.value.indexOf(",") == -1) {
-            isComma = false;
-        }
-        return false;
-    }
-
-    var digits = number.split("");
-    for (var i = 0; i < digits.length; i++) {
-        if (digits[i] == "," && isComma == false) {
-            isComma = true;
-            return true;
-        } else if (digits[i] == "," && isComma == true) {
-            return -1;
-        }
-
-        let digitValue = getDigitValue(digits[i]);
-
-        if (digitValue >= selectedBase || digitValue == -1) {
-            return -1;
-        }
-    }
-    return number;
-}
