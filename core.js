b565d7e2f08bff5a6b976f6ea6e19a4f08db63c1
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-09-11 20:37:49 +0200

    Use 2 spaces as indetation

--- core.js
+++ core.js
@@ -7,90 +7,96 @@ var Base = {
 
 		to = Array.isArray(to) ? to : [to];
 		if (Base.validateNumber(number, from)) {
-		    if (from == 10) {
-		        for (var i = 0; i < to.length; i++) {
-		            results[i] = fromBaseTen(number, to[i]);
-		        }
-		    } else {
-		        let baseTenNumber = toBaseTen(number, from);
-
-		        for (var i = 0; i < to.length; i++) {
-		            results[i] = fromBaseTen(baseTenNumber, to[i]);
-		        }
+		  if (from == 10) {
+		    for (var i = 0; i < to.length; i++) {
+		      results[i] = fromBaseTen(number, to[i]);
 		    }
+		  } else {
+		    let baseTenNumber = toBaseTen(number, from);
+
+		    for (var i = 0; i < to.length; i++) {
+		      results[i] = fromBaseTen(baseTenNumber, to[i]);
+		    }
+		  }
 		} else {
-		    return false;
+		  return false;
 		}
 		return results;
 
 		function toBaseTen(number, base) {
-		    var digits = [],
-		        sum = 0;
-		    var split = number.split(",");
+	    var digits = [],
+	        sum = 0;
+	    var split = number.split(",");
 
-		    for (var i = 0; i < split[0].length; i++) {
-		        sum += Base.getDigitValue(split[0][i]) * Math.pow(base, split[0].length - 1 - i);
-		    }
+	    for (var i = 0; i < split[0].length; i++) {
+	      sum += Base.getDigitValue(split[0][i]) * Math.pow(base, split[0].length - 1 - i);
+	    }
 
-		    if (split[1]) {
-		        sum += ',';
-		        var temp = 0;
-		        for (var i = 0; i < split[1].length; i++) {
-		            temp += Base.getDigitValue(split[1][i]) * Math.pow(base, -(i + 1));
-		        }
-		        sum += temp.toString().substring(2);
-		    }
+	    if (split[1]) {
+	      sum += ',';
+	      var temp = 0;
+	      for (var i = 0; i < split[1].length; i++) {
+	        temp += Base.getDigitValue(split[1][i]) * Math.pow(base, -(i + 1));
+	      }
+	      sum += temp.toString().substring(2);
+	    }
 
-		    return sum.toString();
+	    return sum.toString();
 		}
 
 		function fromBaseTen(number, base) {
-		    var rest = '',
-		        result = '',
-		        split = number.toString().split(",");
+      var rest = '',
+          result = '',
+          split = number.toString().split(",");
 
-		    // Calculate the natural number part of received number.
-		    // e.g. 123 from 123,7482
-		    rest = split[0];
-		    rest = Number(rest);
-		    while (rest != 0) {
-		        result = Base.getNumberCharacter(rest%base) + result;
-		        rest = Math.floor(rest/base);
+      // Calculate the natural number part of received number.
+      // e.g. 123 from 123,7482
+      rest = split[0];
+      rest = Number(rest);
+      while (rest != 0) {
+        result = Base.getNumberCharacter(rest%base) + result;
+        rest = Math.floor(rest/base);
+      }
+
+      // calculates the comma of received number if any
+      // e.g. 0,7482 from 123,7482
+      if (split[1]) {
+        var help = 0;
+        ergebnis += ",";
+        rest = Number('0.' + split[1]);
+        for (var i = 1; i <= 8; i++) {
+          if (rest == 0 || (rest - Math.pow(base, -i) < 0)) {
+            ergebnis += '0';
+          } else {
+            help = Math.floor(rest / Math.pow(base, -i));
+            ergebnis += Base.getNumberCharacter(help).toString();
+            rest = rest % Math.pow(base, -i);
+          }
 		    }
+		  }
 
-		    // calculates the comma of received number if any
-		    // e.g. 0,7482 from 123,7482
-		    if (split[1]) {
-		      var help = 0;
-		        ergebnis += ",";
-		        rest = Number('0.' + split[1]);
-		        for (var i = 1; i <= 8; i++) {
-		            if (rest == 0 || (rest - Math.pow(base, -i) < 0)) {
-		                ergebnis += '0';
-		            } else {
-		                help = Math.floor(rest / Math.pow(base, -i));
-		                ergebnis += Base.getNumberCharacter(help).toString();
-		                rest = rest % Math.pow(base, -i);
-		            }
-		        }
-		    }
-
-		    return result;
-    	}
-    },
+		  return result;
+    }
+  },
 
 
-	/** This function gets a base 10 number and return the corresponding digit to that value.
-	 * E.g. 'E' for 14
+	/** This function gets a base 10 number and returns the character that corresponds to that value
+	 * E.g. 14 returns 'E'; 8 returns '8'
 	 */
 	getNumberCharacter (number) {
-    	return Base.digits[number];
+    return Base.digits[number];
 	},
 
+  /** This function gets a digit (from whatever base) and returns the corresponding value as base 10
+	 * E.g. 'E' returns 14; '8' returns 8
+	 */
 	getDigitValue (digit) {
-    	return Base.digits.indexOf(digit.toUpperCase());
+    return Base.digits.indexOf(digit.toUpperCase());
 	},
 
+  /** Takes a number and validates it
+	 * Returns true if argument is a number, false if not
+	 */
 	validateNumber (number, base) {
 		var isComma = isComma || false;
 		var digits = number.split("");
@@ -123,6 +129,6 @@ var Base = {
 		return true;
 	},
 
-	// Array that maps all the digits from bases 2-36
+	// Array that maps all the digits from base 2 to 36
 	digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 };
--- main.js
+++ main.js
@@ -9,10 +9,10 @@ var addedCharacter = "",
     inputField = document.getElementById('number');
 
 window.onload = function() {
-    base.value = 10;
-    selectedBase = 10;
-    inputField.value = "";
-    updateResultBoxArray();
+  base.value = 10;
+  selectedBase = 10;
+  inputField.value = "";
+  updateResultBoxArray();
 };
 
 // Initialize EventListener
@@ -29,114 +29,112 @@ inputField.addEventListener("keypress", keyInput);
 inputField.addEventListener("input", inputHandler);
 
 function keyInput(e) {
-    var addedCharacter = (e.charCode == 0) ? '-1' : String.fromCharCode(e.charCode);
+  var addedCharacter = (e.charCode == 0) ? '-1' : String.fromCharCode(e.charCode);
 
-    if (!e.ctrlKey) {
-        if (!Base.validateNumber(addedCharacter)) {
-            console.error("Character not allowed. Use digits 0-" + (selectedBase - 1));
-            e.preventDefault();
-        }
+  if (!e.ctrlKey) {
+    if (!Base.validateNumber(addedCharacter)) {
+      console.error("Character not allowed. Use digits 0-" + (selectedBase - 1));
+        e.preventDefault();
     }
+  }
 }
 
 // NOTE: keyInput() does get triggered before inputHandler()
 function inputHandler(e) {
-    // create results
-    let results = Base.convert(selectedBase, resultBoxBase, this.value);
+  // create results
+  let results = Base.convert(selectedBase, resultBoxBase, this.value);
 
-    // add results to the page
-    for (var i = 0; i < resultBox.length; i++) {
-        resultBox[i].innerHTML = results[i];
-    }
+  // add results to the page
+  for (var i = 0; i < resultBox.length; i++) {
+    resultBox[i].innerHTML = results[i];
+  }
 }
 
 function updateBase(e) {
-    if (inputField.value != "") {
-        inputField.value = Base.convert(selectedBase, this.value, inputField.value);
-    }
+  if (inputField.value != "") {
+    inputField.value = Base.convert(selectedBase, this.value, inputField.value);
+  }
 
-    // Update to new Base
-    selectedBase = this.value;
-    inputField.placeholder = "Input (0-" + Base.getNumberCharacter(selectedBase - 1) + ")";
+  // Update to new Base
+  selectedBase = this.value;
+  inputField.placeholder = "Input (0-" + Base.getNumberCharacter(selectedBase - 1) + ")";
 
-    document.querySelector('label[for="base"]').innerHTML = selectedBase;
+  document.querySelector('label[for="base"]').innerHTML = selectedBase;
 }
 
 function handlePaste(e) {
-    var pastedData = e.clipboardData.getData('text');
+  var pastedData = e.clipboardData.getData('text');
 
-    if (!Base.validateNumber(pastedData, selectedBase)) {
-        console.error("Pasted data is not allowed. Try to change the format");
-        e.preventDefault();
-    }
+  if (!Base.validateNumber(pastedData, selectedBase)) {
+    console.error("Pasted data is not allowed. Try to change the format");
+    e.preventDefault();
+  }
 }
 
 function removeResultBox(elem) {
-    elem.parentNode.parentNode.removeChild(elem.parentNode);
-    updateResultBoxArray();
+  elem.parentNode.parentNode.removeChild(elem.parentNode);
+  updateResultBoxArray();
 }
 
 function addResultBox(base) {
-    var newListItem = document.createElement('LI'),
-        baseElement = document.createElement('DIV'),
-        numberElement = document.createElement('DIV'),
-        deleteButton = document.createElement('DIV'),
-        deleteImage = document.createElement('IMG'),
-        dividerImage = document.createElement('IMG'),
-        textNode = document.createTextNode(base + '');
+  var newListItem = document.createElement('LI'),
+      baseElement = document.createElement('DIV'),
+      numberElement = document.createElement('DIV'),
+      deleteButton = document.createElement('DIV'),
+      deleteImage = document.createElement('IMG'),
+      dividerImage = document.createElement('IMG'),
+      textNode = document.createTextNode(base + '');
 
-    deleteImage.setAttribute('src', 'delete.png');
-    deleteButton.appendChild(deleteImage);
-    deleteButton.setAttribute('class', 'delete-button');
-    deleteButton.setAttribute('onclick', 'removeResultBox(this);');
+  deleteImage.setAttribute('src', 'delete.png');
+  deleteButton.appendChild(deleteImage);
+  deleteButton.setAttribute('class', 'delete-button');
+  deleteButton.setAttribute('onclick', 'removeResultBox(this);');
 
-    baseElement.setAttribute('class', 'result-base');
-    baseElement.appendChild(textNode);
-    numberElement.setAttribute('class', 'result-number');
-    dividerImage.setAttribute('src', 'divider-arrow.png');
-    dividerImage.setAttribute('class', 'divider-arrow');
+  baseElement.setAttribute('class', 'result-base');
+  baseElement.appendChild(textNode);
+  numberElement.setAttribute('class', 'result-number');
+  dividerImage.setAttribute('src', 'divider-arrow.png');
+  dividerImage.setAttribute('class', 'divider-arrow');
 
-    newListItem.appendChild(baseElement);
-    newListItem.appendChild(dividerImage);
-    newListItem.appendChild(numberElement);
-    newListItem.appendChild(deleteButton);
+  newListItem.appendChild(baseElement);
+  newListItem.appendChild(dividerImage);
+  newListItem.appendChild(numberElement);
+  newListItem.appendChild(deleteButton);
 
-    numberElement.innerHTML = Base.convert(selectedBase, base, inputField.value);
-    document.getElementsByClassName('results')[0].appendChild(newListItem);
+  numberElement.innerHTML = Base.convert(selectedBase, base, inputField.value);
+  document.getElementsByClassName('results')[0].appendChild(newListItem);
 }
 
 function updateResultBoxArray() {
-    for (var i = 0; i < resultBox.length; i++) {
-        resultBoxBase[i] = document.getElementsByClassName('result-base')[i].innerHTML;
-
-        // NOTE: At some point there could be the need for a config file, to allow changes in the markup let the code untouched
-    }
+  for (var i = 0; i < resultBox.length; i++) {
+    resultBoxBase[i] = document.getElementsByClassName('result-base')[i].innerHTML;
+  }
 }
 
 function addBase(e) {
-    var base = document.getElementById('new-base').value.split(','),
-        length = base.length;
+  var base = document.getElementById('new-base').value.split(','),
+      length = base.length;
 
-    for (let i = 0; i < length; i++) {
-        let help = Base.getNumberCharacter(base[i]);
-        if (help && help != -1) {
-            console.log(Base.getNumberCharacter(base[i]));
-            addResultBox(base[i]);
-        } else {
-            let help = Base.getDigitValue(base[i]);
+  for (let i = 0; i < length; i++) {
+    let help = Base.getNumberCharacter(base[i]);
+    if (help && help != -1) {
+      console.log(Base.getNumberCharacter(base[i]));
+      addResultBox(base[i]);
+    } else {
+      let help = Base.getDigitValue(base[i]);
 
-            if (help && help >= 2) {
-                addResultBox(Base.getDigitValue(base[i])+1);
-            }
-        }
+      if (help && help >= 2) {
+        addResultBox(Base.getDigitValue(base[i])+1);
+      }
     }
+  }
 
-    updateResultBoxArray();
-    document.getElementById('new-base').value = "";
+  updateResultBoxArray();
+  document.getElementById('new-base').value = "";
 
-    document.getElementById('res').scrollTo({
-  top: 8000, 
-  left: 0,
-  behavior: 'smooth'
-});
+  document.getElementById('res').scrollTo({
+    top: 8000,
+    left: 0,
+    behavior: 'smooth'
+  });
 }
