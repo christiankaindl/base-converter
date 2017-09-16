bea730dc2b01a9658845dc6870ed085ed9f5813b
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-09-16 21:32:05 +0200

    Add footer, improve styling, 'add base' button

--- src/main.js
+++ src/main.js
@@ -5,11 +5,10 @@ const {h, app} = hyperapp;
 // Component
 const resultBox = (number, base, targetBase, remove) => {
   return h("li", {class: "result-box"},[
-    h("div", {class: "result-box-base"}, targetBase),
+    h("div", {class: "result-box-base", base: targetBase}, targetBase + " →"),
     h("div", {class: "result-box-number"}, Base.convert(base, targetBase, number)),
     h("button", {class: "result-box-delete-button", onclick: (e) => {remove(e.target)}})
   ]);
-
 }
 
 const emit = app({
@@ -18,13 +17,14 @@ const emit = app({
     number: 123,
     base: 10,
     error: false,
-    outputTargets: [2, 8, 16]
+    outputTargets: [2, 8, 16],
+    newBase: ""
   },
 
   view: (state, actions) => {
     return h("div", {id: "hyperapp"}, [
+      h("p", {id: "description"}, "Convert numbers between numerical systems. Simply start by typing a number."),
       h("section", {id: "input"}, [
-        h("h1", {}, "Convert numbers between numerical systems. Simply start by typing a number:"),
         h("label", {for: "number"}, "Number"),
         h("input", {oninput: (e) => emit("input", e.target.value), placeholder: "Stay cool!", class: state.error?"error":"", id: "input-number", type: "text", name: "number", autofocus: "", value: state.number}),
         h("span", {}, [
@@ -37,9 +37,12 @@ const emit = app({
       ),
       h("section", {id: "results"}, [
         h("h1", {}, "Results"),
+        h("div", {id: "add-base"}, [
+          h("input", {value: state.newBase, type: "text", oninput: e => {return {newBase: e.target.value};}, onkeyup: e => {e.keyCode==13 ? actions.addResultBox(e) : "";}}),
+          h("button", {onclick: actions.addResultBox}, " New base")
+        ]),
         h("ul", {}, state.outputTargets.map((i) => { return resultBox(state.number, state.base, i, actions.removeResultBox);}))
-      ]),
-      h("button", {onclick: (e) => {actions.addResultBox()}}, "ADD")
+      ])
     ]);
   },
 
@@ -56,13 +59,16 @@ const emit = app({
         number: Base.convert(state.base, base, state.number)
       };
     },
-    addResultBox(state) {
+    addResultBox(state, actions, e) {
+      var inputField = e.target.parentElement.getElementsByTagName("input")[0];
+
+      state.outputTargets.push(inputField.value);
       return {
         outputTargets: state.outputTargets
       }
     },
     removeResultBox(state, actions, target) {
-      var base = target.parentElement.getElementsByClassName('result-box-base')[0].textContent,
+      var base = target.parentElement.getElementsByClassName('result-box-base')[0].getAttribute("base"),
         position = state.outputTargets.indexOf(Number(base));
 
       // splice() modyfies the array directly and returns the deleted element
@@ -72,6 +78,12 @@ const emit = app({
         outputTargets: state.outputTargets
       };
     },
+    addBase() {
+      console.log("blub");
+      return {
+        addBaseDialog: true
+      };
+    }
   },
   events: {
     input(state, {setInput: setInput, invalidInput: invalidInput}, number) {
