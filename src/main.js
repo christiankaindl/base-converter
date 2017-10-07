31a8c92960dea23147648707094b019929aaf226
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-10-07 13:50:06 +0200

    Make actions more verbose

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const resultBox = (number, base, targetBase) => {
 
 // Component
 const baseDropdown = (state, actions, type) => {
-  return h("select", {onchange: (e) => {actions.setBase({base: e.target.value, type: type})}}, [
+  return h("select", {onchange: (e) => {actions.updateBase({newBase: e.target.value, type: type})}}, [
     h("option", {value: 2}, "Base 2"),
     h("option", {value: 3}, "Base 3"),
     h("option", {value: 4}, "Base 4"),
@@ -21,13 +21,13 @@ const baseDropdown = (state, actions, type) => {
     h("option", {value: 7}, "Base 7"),
     h("option", {value: 8}, "Base 8"),
     h("option", {value: 9}, "Base 9"),
-    h("option", {value: 10, selected: (type!=="base" ? "selected" : "")}, "Base 10"),
+    h("option", {value: 10, selected: (type=="from" ? "selected" : "")}, "Base 10"),
     h("option", {value: 11}, "Base 11"),
     h("option", {value: 12}, "Base 12"),
     h("option", {value: 13}, "Base 13"),
     h("option", {value: 14}, "Base 14"),
     h("option", {value: 15}, "Base 15"),
-    h("option", {value: 16, selected: (type=="base" ? "selected" : "")}, "Base 16"),
+    h("option", {value: 16, selected: (type=="to" ? "selected" : "")}, "Base 16"),
     h("option", {value: 17}, "Base 17"),
     h("option", {value: 18}, "Base 18"),
     h("option", {value: 19}, "Base 19"),
@@ -63,21 +63,20 @@ app({
   view: (state, actions) => {
     return h("div", {id: "hyperapp"}, [
       h("section", {id: "input"}, [
-        h("span", {contenteditable: "true", oninput: (e) => {
-          var number = e.target.textContent;
-          var error = !Base.validateNumber(number, state.base);
-          actions.setInput({
-            number: number || 0,
-            error: error
-          });
-        }, placeholder: "You are awesome!", class: state.error?"error":"", id: "input-number", type: "text", name: "number", autofocus: "autofocus", value: state.number}, state.number),
+        h("span", {
+          contenteditable: "true",
+          oninput: (e) => actions.input(e.target.textContent),
+          placeholder: "You are awesome!",
+          class: state.error?"error":"",
+          id: "input-number",
+          autofocus: "autofocus"}, state.number),
         h("sub", {}, state.base)]
       ),
       h("div", {id: "from-to"}, [
         h("span", {}, "from"),
-        baseDropdown(state, actions, "number"),
+        baseDropdown(state, actions, "from"),
         h("span", {}, "to"),
-        baseDropdown(state, actions, "base")
+        baseDropdown(state, actions, "to")
       ]),
       h("section", {id: "results"}, [
         h("span", {}, h("b", {}, state.number, h("sub", {}, state.base)), " reults in"),
@@ -87,23 +86,28 @@ app({
   },
 
   actions: {
-    setInput(state, actions, {number, error = false}) {
+    input(state, actions, number) {
       return {
-        number: number,
-        error: error
+        number: number || 0,
+        error: !Base.validateNumber(number, state.base)
       };
     },
-    setBase(state, actions, {base, type}) {
-      return type=="base" ? (() => {
-        state.outputTargets.unshift(Number(base));
+    updateBase(state, actions, {newBase, type}) {
+      if (type === "to") {
+        state.outputTargets.unshift(newBase);
         return {
           outputTargets: state.outputTargets
-        }
-      })() : {
-        number: Base.convert(state.base, base, state.number),
-        base: base
-      };
+        };
+      }
 
+      else if (type === "from") {
+        return {
+          number: Base.convert(state.base, newBase, state.number),
+          base: newBase
+        };
+      }
+
+      console.error("Missig base type. It can be one of the following: 'to' or 'from'");
     }
   },
 
